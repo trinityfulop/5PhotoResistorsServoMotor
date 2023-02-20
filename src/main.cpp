1224ab1d42e57b9cd2a46b399b8dbf73ec4f6f28
TRINITY FULOP <TLF19@FSU.EDU>
2023-02-20 11:27:23 -0500

    reading light sensor 1, 2, 3 values

--- src/main.cpp
+++ src/main.cpp
@@ -13,36 +13,14 @@ void setup() {
 }
 
 void loop() {
-  // Sensor 1
+
   int lightValueSensor1 = analogRead(A0);
-  // if it's below the threshold?
-  if (lightValueSensor1 >= threshold) {
-     if (lastlightValueSensor1 < threshold) {
-        // the sensor just crossed the threshold
-        Serial.println("Light Value right sensor 1 crossed the threshold");
-     }
-  }
-  lastlightValueSensor1 = lightValueSensor1;
-
-    // Sensor 2
   int lightValueSensor2 = analogRead(A1);
-  // if it's below the threshold?
-  if (lightValueSensor2 >= threshold) {
-     if (lastlightValueSensor2 < threshold) {
-        // the sensor just crossed the threshold
-        Serial.println("Light Value center sensor 2 crossed the threshold");
-     }
-  }
-  lastlightValueSensor2 = lightValueSensor2;
-
-    // Sensor 3
   int lightValueSensor3 = analogRead(A2);
-  // if it's below the threshold?
-  if (lightValueSensor3 >= threshold) {
-     if (lastlightValueSensor3 < threshold) {
-        // the sensor just crossed the threshold
-        Serial.println("Light Value right sensor 1 crossed the threshold");
-     }
-  }
-  lastlightValueSensor3 = lightValueSensor3;
+  
+  Serial.print(lightValueSensor1);
+  Serial.print(",");
+  Serial.print(lightValueSensor2);
+  Serial.print(",");
+  Serial.println(lightValueSensor3);
 }
