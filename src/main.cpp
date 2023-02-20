50d3446fd54fe2d06dccef2dab971f9c93b854a1
TRINITY FULOP <TLF19@FSU.EDU>
2023-02-20 11:00:33 -0500

    photresistor threshold

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,48 @@
 #include <Arduino.h>
-#include <Servo.h>
 
-Servo servo1;
-int pos = 0;
-int lastSensorState = HIGH;   // dark to light
-int threshold = 0-1023;   // what is the threshold for light?
+int lastlightValueSensor1 = 700;
+int lastlightValueSensor2 = 700;
+int lastlightValueSensor3 = 700;
+int threshold = 800;   // what is the threshold for light?
 
 void setup() {
   Serial.begin(9600);
-  pinMode (A0, INPUT_PULLUP);
-  servo1.attach(9);
+  pinMode (A0, INPUT);
+  pinMode (A1, INPUT);
+  pinMode (A2, INPUT);
 }
 
 void loop() {
-  // read the sensor:
-  int lightValue = analogRead(A0);
-  int lastlightValue = lightValue; 
-
-  lightValue = map (lightValue, 0, 1023, 0, 180);
- 
+  // Sensor 1
+  int lightValueSensor1 = analogRead(A0);
   // if it's below the threshold?
-  if (lightValue >= threshold) {
-     if (lastlightValue < threshold) {
+  if (lightValueSensor1 >= threshold) {
+     if (lastlightValueSensor1 < threshold) {
         // the sensor just crossed the threshold
-        Serial.println("Light Value crossed the threshold");
+        Serial.println("Light Value right sensor 1 crossed the threshold");
      }
   }
-  lastlightValue = lightValue;
+  lastlightValueSensor1 = lightValueSensor1;
 
-  servo1.write (lightValue);
+    // Sensor 2
+  int lightValueSensor2 = analogRead(A1);
+  // if it's below the threshold?
+  if (lightValueSensor2 >= threshold) {
+     if (lastlightValueSensor2 < threshold) {
+        // the sensor just crossed the threshold
+        Serial.println("Light Value center sensor 2 crossed the threshold");
+     }
+  }
+  lastlightValueSensor2 = lightValueSensor2;
+
+    // Sensor 3
+  int lightValueSensor3 = analogRead(A2);
+  // if it's below the threshold?
+  if (lightValueSensor3 >= threshold) {
+     if (lastlightValueSensor3 < threshold) {
+        // the sensor just crossed the threshold
+        Serial.println("Light Value right sensor 1 crossed the threshold");
+     }
+  }
+  lastlightValueSensor3 = lightValueSensor3;
 }
