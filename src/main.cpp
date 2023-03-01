26dfbd4941c3a93cce1669e9e6e4774afdae7be4
TRINITY FULOP <TLF19@FSU.EDU>
2023-03-01 10:28:51 -0500

    finding the min light value - minSensor

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ void setup() {
 
 void loop() {
 
+// Reading the photresistors
   int lightValueSensor1 = analogRead(A0);
   int lightValueSensor2 = analogRead(A1);
   int lightValueSensor3 = analogRead(A2);
@@ -26,5 +27,30 @@ void loop() {
   Serial.print(lightValueSensor4);
   Serial.print(",");
   Serial.println(lightValueSensor5);
-  delay (3000);
+
+// Finding the lowest light value, telling me the sensor.
+int minVal = 999;
+int minSensor;
+
+if (lightValueSensor1 < minVal) {
+  minVal = lightValueSensor1;
+  minSensor = 1;
+}
+if (lightValueSensor2 < minVal) {
+  minVal = lightValueSensor2;
+  minSensor = 2;
+}
+if (lightValueSensor3 < minVal) {
+  minVal = lightValueSensor3;
+  minSensor = 3;
+}
+if (lightValueSensor4 < minVal) {
+  minVal = lightValueSensor4;
+  minSensor = 4;
+}
+if (lightValueSensor5 < minVal) {
+  minVal = lightValueSensor5;
+  minSensor = 5;
+}
+
 }
